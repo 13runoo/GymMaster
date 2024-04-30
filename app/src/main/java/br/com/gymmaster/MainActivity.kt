47e7411bc5225f061492b1770ad0e6a83f2cf77e
13runoo <brno20045@gmail.com>
2024-04-30 16:59:59 -0300

    ajustes layout

--- app/src/main/java/br/com/gymmaster/MainActivity.kt
+++ app/src/main/java/br/com/gymmaster/MainActivity.kt
@@ -11,13 +11,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import br.com.gymmaster.ui.theme.GymMasterTheme
+import br.com.gymmaster.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GymMasterTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Login()
