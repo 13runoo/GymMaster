429acbb5849f9488ef68341e9dd9423a19372ce5
13runoo <brno20045@gmail.com>
2024-05-02 16:45:33 -0300

    ajustes layout

--- app/src/main/java/br/com/gymmaster/MainActivity.kt
+++ app/src/main/java/br/com/gymmaster/MainActivity.kt
@@ -1,16 +1,21 @@
 package br.com.gymmaster
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.gymmaster.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +25,7 @@ class MainActivity : ComponentActivity() {
             AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Login()
+                    Week()
                 }
             }
         }
@@ -115,6 +120,208 @@ fun Login() {
 
 
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Register() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+
+                }
+            )
+        }
+    ) { paddingValues ->
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        Column(
+            Modifier.fillMaxSize().padding(
+                vertical = paddingValues.calculateTopPadding(),
+                horizontal = 48.dp
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Image(painterResource(R.drawable.logo_gym), "Logo Do Aplicativo")
+            Column {
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Nome") }
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Sobrenome") }
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email") }
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Senha") }
+                )
+
+            }
+            Spacer(Modifier.height(25.dp))
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                Spacer(Modifier.width(24.dp))
+                OutlinedButton(onClick = { }) {
+                    Text("Cadastrar-se")
+                }
+
+            }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun Edit() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+
+                }
+            )
+        }
+    ) { paddingValues ->
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        Column(
+            Modifier.fillMaxSize().padding(
+                vertical = paddingValues.calculateTopPadding(),
+                horizontal = 48.dp
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Image(painterResource(R.drawable.logo_gym), "Logo Do Aplicativo")
+            Column {
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Alterar Nome") }
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Alterar Sobrenome") }
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Alterar Email") }
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Alterar Senha") }
+                )
+
+            }
+            Spacer(Modifier.height(25.dp))
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                Spacer(Modifier.width(24.dp))
+                OutlinedButton(onClick = { }) {
+                    Text("Cadastrar-se")
+                }
+
+            }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Week() {
+    Scaffold(
+
+
+    ) { paddingValues ->
+        Column(
+            Modifier.fillMaxSize().padding(
+                vertical = paddingValues.calculateTopPadding(),
+                horizontal = 16.dp
+            )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(onClick = {}) {
+                    Icon(Icons.Default.Menu, "Navegação")
+                }
+                Image(painterResource(R.drawable.logo_gym), "Logo Do Aplicativo", modifier = Modifier.size(90.dp))
+                Icon(
+                    Icons.Default.AccountCircle,
+                    "Account icon",
+                )
+
+            }
+            Column(
+                Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Dias de treinos na semana", fontSize = 30.sp)
+
+                WeekButton("Segunda-Feira",{})
+                WeekButton("Terça-Feira",{})
+                WeekButton("Quarta-Feira",{})
+                WeekButton("Quinta-Feira",{})
+                WeekButton("Sexta-Feira",{})
+                WeekButton("Sábado",{})
+                WeekButton("Domingo",{})
+
+
+
+
+            }
+        }
+    }
+
+}
+
+@Composable
+private fun WeekButton(text:String, onClick: ()->Unit) {
+    TextButton(
+        onClick = onClick
+    ) {
+        Text(text, fontSize = 30.sp)
+    }
+}
+
+
+
+
+
+
+
 
 
 
